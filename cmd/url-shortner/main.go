9090bc992a5694a8c71067c1a305cec128a09d51
Slava Mashkov <mashkov.vd@gmail.com>
2024-02-07 22:47:56 +0300

    Add redirect handler

--- cmd/url-shortner/main.go
+++ cmd/url-shortner/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handler/url/redirect"
 	"url-shortener/internal/http-server/handler/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
 	"url-shortener/internal/lib/logger/handlers/slogpretty"
@@ -47,6 +48,7 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/api/v1/save", save.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
---
+++ internal/http-server/handler/url/redirect/redirect.go
@@ -0,0 +1,58 @@
+package redirect
+
+import (
+	"errors"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log/slog"
+	"net/http"
+	"url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+)
+
+//go:generate go run github.com/vektra/mockery/v2@v2.40.1 --name=URLGetter
+type URLGetter interface {
+	GetURLByAlias(alias string) (string, error)
+}
+
+func New(log *slog.Logger, urlGetter URLGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "handler.url.redirect.New"
+
+		log = log.With(
+			slog.String("fn", fn),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.JSON(w, r, response.Error("invalid request"))
+
+			return
+		}
+
+		resURL, err := urlGetter.GetURLByAlias(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", slog.String("alias", alias))
+
+			render.JSON(w, r, response.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to get url by alias", sl.Err(err), slog.String("alias", alias))
+
+			render.JSON(w, r, response.Error("internal error"))
+
+			return
+		}
+
+		log.Info("got url", slog.String("url", resURL))
+
+		http.Redirect(w, r, resURL, http.StatusFound)
+	}
+}
